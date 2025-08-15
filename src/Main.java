44363dbe330843317ca98ddc696193b949f18fc7
Alina <al89056667426@gmail.com>
2025-08-15 09:23:30 +0300

    modification 1

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,20 @@ public class Main {
     public static void main(String[] args) {
         // Назначение переменных для хранения стоимости билета
         int startBalance = 100; // сумма начального баланса
-        int replenishment = 1000; // сумма пополнения
+        int replenishment = 1100; // сумма пополнения
         int bonus = 0; // количество бонусных рублей
-        int finalBalance = startBalance + replenishment + bonus; // итоговый баланс
 
-        // Расчёт бонусов
-        if(replenishment >= 1000) {
-            bonus = replenishment/100;
+
+        // Условия получения бонусов
+        if (replenishment >= 1000) {
+            bonus = replenishment / 100;
         }
 
+        // Расчёт бонусов
+        int finalBalance = startBalance + replenishment + bonus;
+
         // Вывод результатов
-        System.out.println ("Бонус равен " + bonus + " руб."); // вывод количества бонусов
+        System.out.println("Бонус равен " + bonus + " руб."); // вывод количества бонусов
         System.out.println("Баланс равен " + finalBalance + " руб."); // вывод баланса
     }
 }
